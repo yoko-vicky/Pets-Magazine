48e3893c8c3e569df44055674db7cf4478c6bafc
yocosaka <sakayoco55@gmail.com>
2021-01-25 23:40:12 +0900

    Add routs in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'categories#index'
+  resources :articles do
+    resources :votes, only: %i[create destroy]
+  end
+  resources :categories, except: %i[edit update destroy]
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'my_favorites', to: 'articles#favorites'
 end
