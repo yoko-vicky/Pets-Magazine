048f32fad994f0964b628b7a8546c8286cd8be62
yocosaka <sakayoco55@gmail.com>
2021-01-26 16:58:54 +0900

    Update active_storage.service in config/environments and Add s3 info to storage.yml - Add 'aws-sdk', '~> 3' and "aws-sdk-s3", require: false gems - Add amazon credentials using Rails credentials - Add credentials info to heroku config:set as well - Update active_storage.service to amazon - Add migration by running 'rails active_storage:install:migrations'

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'font-awesome-sass', '~> 5.15.1'
 gem 'paperclip'
 gem 'sorcery'
 
+# Add s3
+gem 'aws-sdk', '~> 3'
+gem "aws-sdk-s3", require: false
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = false
     config.cache_store = :null_store
   end
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
